d1712c161c068c56b361bc2468c58f7d93f981d3
Isaías Ramos <100144617+isaiasramosdeoliveira@users.noreply.github.com>
2022-08-06 19:27:27 -0300

    Mudanças na validação do form.

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -26,14 +26,34 @@ var Valid = /*#__PURE__*/function () {
   _createClass(Valid, [{
     key: "validData",
     value: function validData(nome, sobrenome, senha, idade, msg) {
-      if (nome === "" || sobrenome === "" || senha === "" || idade === "") {
+      if (nome === "" && sobrenome === "" && senha === "" && idade === "") {
         msg.style.backgroundColor = "#D85050";
         msg.style.color = "#8C1313";
         msg.innerText = "Preencha os campos.";
       }
 
       if (nome === "") {
+        msg.style.backgroundColor = "#D85050";
+        msg.style.color = "#8C1313";
         msg.innerText = "Preencha com seu nome.";
+      }
+
+      if (idade === "") {
+        msg.style.backgroundColor = "#D85050";
+        msg.style.color = "#8C1313";
+        msg.innerText = "Preencha com sua idade.";
+      }
+
+      if (sobrenome === "") {
+        msg.style.backgroundColor = "#D85050";
+        msg.style.color = "#8C1313";
+        msg.innerText = "Preencha com seu sobrenome.";
+      }
+
+      if (senha === "") {
+        msg.style.backgroundColor = "#D85050";
+        msg.style.color = "#8C1313";
+        msg.innerText = "Preencha com sua senha";
       }
 
       if (nome !== "" && sobrenome !== "" && senha !== "" && idade !== "") {
--- src/modules/ValidForm.js
+++ src/modules/ValidForm.js
@@ -5,6 +5,26 @@ export default class Valid{
             msg.style.color = "#8C1313"
             msg.innerText = "Preencha os campos."
         }
+        if(nome === ""){
+            msg.style.backgroundColor = "#D85050"
+            msg.style.color = "#8C1313"
+            msg.innerText = "Preencha com seu nome."
+        } 
+        if(idade === ""){
+            msg.style.backgroundColor = "#D85050"
+            msg.style.color = "#8C1313"
+            msg.innerText = "Preencha com sua idade."
+        }
+        if(sobrenome === ""){
+            msg.style.backgroundColor = "#D85050"
+            msg.style.color = "#8C1313"
+            msg.innerText = "Preencha com seu sobrenome."
+        }
+        if(senha === ""){
+            msg.style.backgroundColor = "#D85050"
+            msg.style.color = "#8C1313"
+            msg.innerText = "Preencha com sua senha"
+        }
         if(nome !=="" && sobrenome!=="" && senha !== "" && idade !== ""){
             msg.style.backgroundColor = "#3EC18F"
             msg.style.color = "#1B895C"
